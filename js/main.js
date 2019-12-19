03e13170d43cdb7a0603e9b1a6fa63e6c0937884
frontend90 <leon44173@gmail.com>
2019-12-19 17:07:43 +0200

    внесены изменения

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-$(document).ready(function(){
+/*********************     slick slider      ***********************/
+$(document).ready(function () {
   $('.header-slider').slick({
     dots: true,
-    autoplay: true
+    autoplay: true,
+    arrows: false
+  });
+});
+
+/*********************     плавный скролл        ************************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
   });
 });
